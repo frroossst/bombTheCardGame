1f7462e840cd194afe32dee02ec946641b8f6b17
home <adhyanpatel@gmail.com>
2022-02-24 19:43:42 -0600

    0.0.2 - build a deck

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from distutils.command.build import build
 import pygame
 
 WIDTH, HEIGHT = 1080, 720
@@ -6,19 +7,37 @@ BLACK = (0,0,0)
 WHITE = (255,255,255)
 FPS = 60
 pygame.display.set_caption("Bomb : The card game")
+DECK = []
 
 
-def main():
+
+def build_deck() -> list:
+    num_cards = ["1","2","3","4","5","6","7","8","9","10"]
+    pic_cards = ["jack","king","queen"]
+    colours = ["heart","spade","diamond","club"]
+    deck = []
+
+    for i in colours:
+        for j in num_cards:
+            deck.append(i + "_" + j + ".png")
+        for k in pic_cards:
+            deck.append(i + "_" + k + ".png")
+
+    return sorted(deck)
+
+
+def main() -> None:
     clock = pygame.time.Clock()
-    run = True
-    
+    run = True    
+    DECK = build_deck()
+    print(DECK)
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
-            WIN.fill(WHITE)
+            WIN.fill(BLACK)
             pygame.display.update()
 
     pygame.quit()
